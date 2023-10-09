4fd7b47e681afc4e97179bb44b061f5236aa798f
Jonserafim7 <142022025+Jonserafim7@users.noreply.github.com>
2023-10-09 20:36:49 -0300

    version 2.0

--- Twimba(Twitter-Clone)/index.js
+++ Twimba(Twitter-Clone)/index.js
@@ -1,13 +1,75 @@
+// data 
 import { tweetsData } from "./data.js";
 
+
+// global variables
 const tweetInput = document.getElementById('tweet-input')
 const tweetBtn = document.getElementById('tweet-btn')
 
+
+
+// event listeners
 tweetBtn.addEventListener('click', function(){
 
     console.log(tweetInput.value)
 
-    tweetInput.value = ""
-
 })
+
+   
+
+// functions
+function renderFeedHTML() {
+
+    document.getElementById('feed').innerHTML = getFeedHtml()
+
+}
+
+renderFeedHTML()
+
+function getFeedHtml() {
+
+    let feedHTML = ""
+
+    tweetsData.forEach(function(tweet){
+
+        feedHTML += `
+
+        <div class="tweet">
+            <div class="tweet-inner">
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                            <i class="fa-regular fa-solid fa-comment-dots"></i>
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-heart"></i>
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            <i class="fa-solid fa-retweet"></i>
+                            ${tweet.retweets}
+                        </span>
+                    </div>   
+                </div>            
+            </div>
+        </div>
+        `
+
+    })
+        
+    return feedHTML
+
+}
+
+
+
+
+
+
+
+
 
