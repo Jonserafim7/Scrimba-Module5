0df1052da9112dafb56ac17a9170129c912c9cf1
Jonserafim7 <142022025+Jonserafim7@users.noreply.github.com>
2023-10-20 11:04:18 -0300

    created the folder for the solo project for this module

--- Mini-Projects/Constructor-challenges/index.js
+++ Mini-Projects/Constructor-challenges/index.js
@@ -1,7 +1,32 @@
-function Character(name) {
-    this.name = name
-    this.collectedItemsArr = []
-    this.addItem = function(item){
+// function Character(name) {
+//     this.name = name
+//     this.collectedItemsArr = []
+//     this.addItem = function(item){
+//         this.collectedItemsArr.push(item)
+//         console.log(`${this.name} now has: ${this.collectedItemsArr.join(', ')}`)
+//     }
+// }
+
+// const dave = new Character("Dave")
+
+// dave.addItem("banana")
+// dave.addItem("orange")
+// dave.addItem("car")
+
+// console.log(dave)
+
+
+
+
+
+// Refactoring the constructor as a class
+
+class Character {
+    constructor (name) {
+        this.name = name
+        this.collectedItemsArr = []
+    }
+    addItem = function(item){
         this.collectedItemsArr.push(item)
         console.log(`${this.name} now has: ${this.collectedItemsArr.join(', ')}`)
     }
