bef550afbc79f4053da787781e9b97da2c3d5976
Jonserafim7 <142022025+Jonserafim7@users.noreply.github.com>
2023-10-25 19:48:01 -0300

    added a user review feature and also a discount feature

--- Solo-Project-Restaurant-ordering-app/index.js
+++ Solo-Project-Restaurant-ordering-app/index.js
@@ -60,6 +60,26 @@ document.addEventListener("click",function(e){
     else if(e.target.dataset.closemodal){
         closePaymentModal()
     }
+    else if(e.target.dataset.starone){
+        console.log("user review: one star")
+        paymentModal.style.display = "none"
+    }
+    else if(e.target.dataset.startwo){
+        console.log("user review: two stars")
+        paymentModal.style.display = "none"
+    }
+    else if(e.target.dataset.starthree){
+        console.log("user review: three stars")
+        paymentModal.style.display = "none"
+    }
+    else if(e.target.dataset.starfour){
+        console.log("user review: four stars")
+        paymentModal.style.display = "none"
+    }
+    else if(e.target.dataset.starfive){
+        console.log("user review: five stars")
+        paymentModal.style.display = "none"
+    }
 })
 
 
@@ -88,14 +108,16 @@ function getItemMenuHTML() {
         const {name,price,uuid, description, icon} = menuItem;
         return `
         <div class="container">
-            <div class="menu-item-content">
-                <div class="menu-item-icon">${icon}</div>
-                <div class="menu-item-details">
-                    <h3 class="menu-item-name">${name}</h3>
-                    <p class="menu-item-description">${description}</p>
-                    <h3 class="menu-item-price">$ ${price}</h3>
+            <div class="menu-container">
+                <div class="menu-item-content">
+                    <div class="menu-item-icon">${icon}</div>
+                    <div class="menu-item-details">
+                        <h3 class="menu-item-name">${name}</h3>
+                        <p class="menu-item-description">${description}</p>
+                        <h3 class="menu-item-price">$ ${price}</h3>
+                    </div>
+                    <button class="add-to-cart-btn" data-addbtn="${uuid}">+</button>
                 </div>
-                <button class="add-to-cart-btn" data-addbtn="${uuid}">+</button>
             </div>
         </div>
         `;
@@ -158,6 +180,10 @@ function findObjectByUUID(menuItemUUID) {
     return cart.items.find(menuItem => menuItem.uuid === menuItemUUID);
 }
 
+function findObjectByName(menuItemName) {
+    return cart.items.find(menuItem => menuItem.name === menuItemName);
+}
+
 function showCart(){
     cartSectionEl.classList.remove("hidden");
 }
@@ -180,15 +206,40 @@ function generateCartHTML() {
         `;
     }).join('');
 
-    const totalPriceHTML = `
+    let hasBeer = findObjectByName("Beer");
+    let hasPizza = findObjectByName("Pizza");
+    let hasHamburger = findObjectByName("Hamburger");
+
+    let discountApplied = (hasBeer && (hasPizza || hasHamburger));
+    let warningMessage = "";
+
+    if (hasBeer) {
+        if (hasPizza || hasHamburger) {
+            warningMessage = "You saved 15%";
+        } else {
+            warningMessage = "Add a Pizza or a Burguer to get 15% off";
+        }
+    } else if (hasPizza || hasHamburger) {
+        warningMessage = "Add a drink to get 15% off";
+    }
+
+    let discountedPrice = discountApplied ? cart.totalPrice - (cart.totalPrice * 0.15) : cart.totalPrice;
+
+    let totalPriceHTML = `
         <div class="cart-total-content">
-            <h3 class="total-price-text">Total price</h3>
-            <h3 class="total-price-value">$ ${cart.totalPrice}</h3>
+            <div>
+                <p class="${discountApplied ? "with-discount-warning" : "no-discount-warning"}">${warningMessage}</p>
+            </div>
+            <div class="cart-total-price">
+                <h3 class="total-price-text">Total price</h3>
+                <h3 class="total-price-value">$ ${discountedPrice}</h3>
+            </div>
         </div>
     `;
 
     return {itemsHTML, totalPriceHTML}
 }
+
 
 function calculateTotalCartValue() {
     return cart.items.reduce((total, menuItem) => total + (menuItem.price * menuItem.quantity), 0);
@@ -222,13 +273,13 @@ paymentForm.addEventListener("submit",function(e) {
     const paymentFormData = new FormData(paymentForm)
 
     const userName = paymentFormData.get("fullName")
-    console.log(userName)
+    // console.log(userName)
 
     const cardNumber = paymentFormData.get("card-numer")
-    console.log(cardNumber)
+    // console.log(cardNumber)
 
     const cardCVV = paymentFormData.get("cvv")
-    console.log(cardCVV)
+    // console.log(cardCVV)
 
 
     modalText.innerHTML = `
@@ -241,29 +292,65 @@ paymentForm.addEventListener("submit",function(e) {
         document.getElementById('uploadText').textContent = `
         Purchase complete!
         `
-    }, 1500)
+    }, 1000)
 
     setTimeout(function(){
-        paymentModal.style.display = "none"
-    },3000)
+        document.getElementById('modal-inner').innerHTML = `
+        <h2 class="modal-inner-headline">Thanks for your purchase! </h2>
+        <h2 class="modal-inner-exp-rate">Rate your experience</h2>
+        <div class="idiot-gif">
+            <i class="fa-solid fa-star" id="star-one" data-starone="onestar"></i>
+            <i class="fa-solid fa-star" id="star-two" data-startwo="twostar"></i>
+            <i class="fa-solid fa-star" id="star-three" data-startree="threestar"></i>
+            <i class="fa-solid fa-star" id="star-four" data-starfour="fourstar"></i>
+            <i class="fa-solid fa-star" id="star-five" data-starfive="fivestar"></i>
+        </div>
+        `
+    }, 1000)
 
+    setTimeout(() => {
+        userRating()
+    }, 1000);
+    
     setTimeout(() => {
         cartSection.innerHTML = `Thanks, ${userName}! Your order is on its way!`
         cartSection.classList.toggle("thanks-message")
-    }, 3000);
-
-    // setTimeout(function(){
-    //     document.getElementById('modal-inner').innerHTML = `
-    //     <h2>Thanks <span class="modal-display-name">${userName}</span>,you sucker! </h2>
-    //     <p>We just sold the rights to your eternal soul.</p>
-    //     <div class="idiot-gif">
-    //         <img src="images/pirate.gif">
-    //     </div>
-    //     `
-    //     modalCloseBtn.disabled = false
-    // }, 3000)
-    
+    }, 1000);
 })
+
+function highlightStar(hoveredStar, changedStar){
+    document.getElementById(hoveredStar).addEventListener("mouseenter", function(){
+        document.getElementById(changedStar).style.color = "#16DB99"
+    }) 
+    
+    document.getElementById(hoveredStar).addEventListener("mouseleave", function(){
+        document.getElementById(changedStar).style.color = "#757575"
+    }) 
+    
+}
+
+function userRating() {
+    highlightStar("star-five", "star-one")
+    highlightStar("star-five", "star-two")
+    highlightStar("star-five", "star-three")
+    highlightStar("star-five", "star-four")
+    highlightStar("star-five", "star-five")
+
+    
+    highlightStar("star-four", "star-one")
+    highlightStar("star-four", "star-two")
+    highlightStar("star-four", "star-three")
+    highlightStar("star-four", "star-four")
+
+    highlightStar("star-three", "star-two")
+    highlightStar("star-three", "star-one")
+    highlightStar("star-three", "star-three")
+
+    highlightStar("star-two", "star-one")
+    highlightStar("star-two", "star-two")
+
+    highlightStar("star-one", "star-one")
+}
 
 
 
